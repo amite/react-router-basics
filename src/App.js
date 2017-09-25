23f543570328f40281ec0933da4f2b22812597ae
amite <amit.erandole@gmail.com>
2017-09-25 11:49:14 +0530

    add one level deep layout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 const HomePage = () => <h1>Home</h1>
 const Dashboard = () => <h1>Dashboard</h1>
 
+const fidgetSpinnersInventory = [
+  { name: 'Batman Batarang Alloy', url: 'batman-alloy', price: 499 },
+  { name: 'Gear Type Pure Metal', url: 'gear-metal', price: 299 },
+  { name: 'Pirates of the Caribbean', url: 'pirates-carribean', price: 1499 }
+]
+
+const FidgetyStoreSubLayout = () => {
+  return (
+    <div>
+      <h1>Fidgety - Fidget Spinner Store</h1>
+      {fidgetSpinnersInventory.map(fidget => {
+        return (
+          <h5>
+            <Link to={`fidgets/${fidget.url}`}>{fidget.name}</Link>
+          </h5>
+        )
+      })}
+    </div>
+  )
+}
+
 const Layout = () => (
   <div className="primary-layout">
     <header>
@@ -18,11 +39,15 @@ const Layout = () => (
         <li>
           <Link to="/dashboard">Dashboard</Link>
         </li>
+        <li>
+          <Link to="/fidgets">Fidgets</Link>
+        </li>
       </ul>
     </nav>
     <main>
       <Route path="/" exact component={HomePage} />
       <Route path="/dashboard" component={Dashboard} />
+      <Route path="/fidgets" component={FidgetyStoreSubLayout} />
     </main>
     <footer>
       <p>Common Footer here</p>
