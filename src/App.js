403f518357979fa6c22f7fcb554f012017272378
amite <amit.erandole@gmail.com>
2017-09-25 10:48:12 +0530

    added basic layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const HomePage = () => <h1>Home</h1>
 const Dashboard = () => <h1>Dashboard</h1>
 
+const Layout = () => (
+  <div className="primary-layout">
+    <header>
+      <h1>Our React Router 4 App</h1>
+    </header>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Dashboard</li>
+      </ul>
+    </nav>
+    <main>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/dashboard" component={Dashboard} />
+    </main>
+    <footer>
+      <p>Common Footer here</p>
+    </footer>
+  </div>
+)
+
 class App extends Component {
   render() {
-    return <h1>Hello Router</h1>
+    return (
+      <Router>
+        <Layout />
+      </Router>
+    )
   }
 }
 
